378da068da79ea6aa44b15633b49a6905e6e94f6
Bruno C. A. Gaudino <brunocesaragweb@gmail.com>
2024-05-28 14:21:35 -0300

    First game version

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ A simple Super Mario web game build it with HTML/CSS/JS.
 ### Version
 | Version           | Description                                                      |
 | ----------------- | ---------------------------------------------------------------- |
-| 1.0.0             | First commit with base files                                     |
+| 1.0.0             | First commit with base files and first game version              |
 
 
--- dev/scripts.js
+++ dev/scripts.js
@@ -1,12 +1,38 @@
 //parou 14m
 const mario = document.querySelector('.mario');
+const buttonUp = document.querySelector('.button-up');
+const pipe = document.querySelector('.pipe');
 
 const jump = () => {
   mario.classList.add('jump');
-
+  
   setTimeout(() => {
     mario.classList.remove('jump');
   },500);
 }
 
+const loop = setInterval(() => {
+
+  const pipePosition = pipe.offsetLeft;
+  const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
+
+  if (pipePosition <= 60 && pipePosition > 0 && marioPosition < 55) {
+
+    pipe.style.animation = 'none';
+    pipe.style.left = `${pipePosition}px`;
+
+    mario.style.animation = 'none';
+    mario.style.bottom = `${marioPosition}px`;
+
+    mario.src = './img/game-over.png';
+    mario.style.width = '40px';
+    mario.style.marginLeft = `20px`;
+
+    clearInterval(loop);
+  
+  }
+
+}, 10);
+
 document.addEventListener('keydown', jump);
+//buttonUp.addEventListener('click', jump);
